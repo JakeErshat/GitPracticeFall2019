461c24919d36b5eebb04e692015e187e10d61aa6
JakeErshat <aileiwa1@umbc.edu>
2020-07-02 20:12:34 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,8 +1,31 @@
 package com.automation.tests.day3;
 
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class FindElementsPractice {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertek.com");
+        WebElement signUp = driver.findElement(By.name("wooden_spoon"));
+        signUp.click();
+        Thread.sleep(2000);
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+        WebElement message = driver.findElement(By.className("subheader"));
+        String actual = message.getText();
+        if (expected.equals(actual)){
+            System.out.println("TEST PASSED");
+        }
+        else{
+            System.out.println("TEST FAILED");
+        }
+        driver.quit();
     }
 }
