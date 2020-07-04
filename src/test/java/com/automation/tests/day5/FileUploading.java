6eaa8c3c5ea26a2f260dbfd0a261934e53e4688f
JakeErshat <aileiwa1@umbc.edu>
2020-07-04 17:17:11 -0400

    day5

---
+++ src/test/java/com/automation/tests/day5/FileUploading.java
@@ -0,0 +1,17 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUploading {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/nested_frames");
+        BrowserUtils.wait(4);
+    }
+}
