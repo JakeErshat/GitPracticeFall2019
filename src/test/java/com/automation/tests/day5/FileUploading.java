5966f2b409f2f327df07dbdd7c69344d683b0f60
JakeErshat <aileiwa1@umbc.edu>
2020-07-04 17:21:48 -0400

    day5

--- src/test/java/com/automation/tests/day5/FileUploading.java
+++ src/test/java/com/automation/tests/day5/FileUploading.java
@@ -2,7 +2,9 @@ package com.automation.tests.day5;
 
 import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FileUploading {
@@ -11,7 +13,15 @@ public class FileUploading {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://practice.cybertekschool.com/nested_frames");
-        BrowserUtils.wait(4);
+        driver.get("http://practice.cybertekschool.com/upload");
+        BrowserUtils.wait(5);
+
+        WebElement upload = driver.findElement(By.id("file-upload"));
+        String filePath = System.getProperty("user.dir") + "/pom.xml";
+
+        upload.sendKeys(filePath);
+
+        BrowserUtils.wait(5);
+        driver.quit();
     }
 }
