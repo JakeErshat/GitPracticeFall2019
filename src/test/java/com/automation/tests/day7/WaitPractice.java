8443dfeb5bfd98685da5088ed16600b51bdfb31b
JakeErshat <aileiwa1@umbc.edu>
2020-07-08 20:58:50 -0400

    day7

--- src/test/java/com/automation/tests/day7/WaitPractice.java
+++ src/test/java/com/automation/tests/day7/WaitPractice.java
@@ -1,4 +1,36 @@
 package com.automation.tests.day7;
 
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class WaitPractice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = DriverFactory.createDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dynamic_controls");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Hello, world!");
+    }
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
 }
